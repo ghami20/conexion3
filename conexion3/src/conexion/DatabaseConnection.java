9a2e00ac9c578985edf0962d9e510a3001c51ad0
Gamaliel Quiroz <ghamiboxbox@gmail.com>
2024-05-14 15:34:10 -0300

    cambios

--- conexion3/src/conexion/DatabaseConnection.java
+++ conexion3/src/conexion/DatabaseConnection.java
@@ -1,4 +1,4 @@
-package Controlador;
+package conexion;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
--- conexion3/src/conexion/Main.java
+++ conexion3/src/conexion/Main.java
@@ -1,9 +1,7 @@
-package Vista;
+package conexion;
 
 import javax.swing.JOptionPane;
 
-import Modelos.Usuario;
-import Controlador.UsuarioControlador;
 public class Main {
 	public static void main(String[] args) {
 		
